6452e5c1dc7b34b4ff10be9f7a04757480fc03d5
Sunmeet Singh <renu.kabeer@gmail.com>
2022-03-05 22:16:18 +0530

    This is the progress till now in Js

---
+++ Module 2/intro.js
@@ -0,0 +1,38 @@
+console.log("Swagat hai aapka, Kya haal hain aapke?");
+let a=10;
+console.log(a);
+let str = 'This is a String';
+let bool = true;
+console.log(str,bool);
+for(i = 0; i<=10; i++){
+    console.log("Sunmeet");
+}
+
+
+let n = 11;
+flag=0
+for (i=2;i*i<=n;i++){
+    if(n%i==0){
+        flag=1;
+    }
+
+
+}
+if (flag==0){
+    console.log("Prime");
+}
+else{
+    console.log("Not prime");
+}
+
+console.log("abc",end='');
+console.log("abc",end='');
+console.log("abc",end='');
+console.log("abc",end='');
+
+
+// for(i=1;i<=5;i++){
+//     for(j=1;j<=i;j++){
+//         console.log("*\t");
+//     }
+// }
